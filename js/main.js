d602c300cf519577a13d40349020d84e4588753f
prashanta0234 <prashanta0234@gmail.com>
2021-07-03 10:35:16 +0600

    project1withboostrap

---
+++ js/main.js
@@ -0,0 +1,10 @@
+// $('.banner').slick({
+//     slidesToShow: 1,
+//   slidesToScroll: 1,
+//   autoplay: true,
+//   autoplaySpeed: 4000,
+//   arrows:true,
+//   prevArrow: '<i class="fas fa-angle-right arrows_left"></i>',
+//   nextArrow: '<i class="fas fa-angle-left arrows_right"></i>',
+  
+// });
